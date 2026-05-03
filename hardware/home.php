38482b6c64b1c12ecf897ad5e68f8c26e423405a
natemon-stack <namontoute@salcc.edu.lc>
2026-05-03 15:21:56 -0400

    Update home.php

--- hardware/home.php
+++ hardware/home.php
@@ -1,11 +1,25 @@
+<?php
+session_start();
+include "connectdb.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PC Upgrade Advisor</title>
+</head>
+    
+<body>
 <h1>PC Upgrade Advisor</h1>
-
+<p>Welcome to the hardware performance system.</p>
+<br>
 <a href="signup.php">
-    <button>Sign Up</button>
+    Sign Up
 </a>
-
 <br><br>
 
-<a href="login.php">
-    <button>Login</button>
+    <a href="login.php">
+    Login
 </a>
+</body>
+</html>
