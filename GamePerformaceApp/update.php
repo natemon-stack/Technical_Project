8d3833b5ad0412d45336e7f0f83ddbb5072bedeb
Nathan k. Montoute <namontoute@salcc.edu.lc>
2026-05-10 20:05:27 -0400

    Update update.php

--- GamePerformaceApp/update.php
+++ GamePerformaceApp/update.php
@@ -7,12 +7,7 @@ $cpu_id = $_POST["cpu_id"];
 $gpu_id = $_POST["gpu_id"];
 $ram_amount = $_POST["ram_amount"];
 
-$sql = "UPDATE user_systems SET
-        cpu_id='$cpu_id',
-        gpu_id='$gpu_id',
-        ram_amount='$ram_amount'
-        WHERE id='$id'
-";
+$sql = "UPDATE user_systems SET cpu_id='$cpu_id',gpu_id='$gpu_id', ram_amount='$ram_amount' WHERE id='$id'";
 
 if($conn->query($sql) === TRUE){
     echo "System updated successfully<br>";
